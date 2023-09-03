78a05f926804aa8b038f4d7f6a14b348d70f55bb
Mahmudul Islam <iamahmudul@gmail.com>
2023-09-03 22:30:05 +0600

    p

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,52 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // 1
+    /* return str.split('').reverse().join(''); */
+    
+    // 2
+    /* if (!str) return str;
+    let rts = "";
+    for (let i = 0, len = str.length - 1; i < len; i++) {
+        rts += str[len - i];
+    }
+    return rts; */
+
+    // 3
+    /* if (!str) return str;
+    let rts = '';
+    for (let char of str) {
+        rts = char + rts;
+    }
+    return rts; */
+
+    // 4
+    /* if (!str) return str;
+    for (let i = 0, len = str.length - 1; i < len; i++, len--) {
+        const temp = str[i];
+        str[i] = str[len - i];
+        str[len - i] = temp;
+    }
+    return str; */
+
+    // 5
+    debugger;
+    return str.split('').reduce((rev, char) => char + rev, '')
+
+    // 6
+    /* const arr = str.split('');
+    let len = arr.length - 1;
+    console.log('arr============', arr);
+    for (let i = 0; i < len; i++, len--) {
+        const temp = arr[i];
+        arr[i] = arr[len - i];
+        arr[len - i] = temp;
+    }
+    console.log('rev arr============', arr);
+    return arr.join(''); */
+}
+
+reverse('asdf');
 
 module.exports = reverse;
